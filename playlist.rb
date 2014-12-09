bf5e2bc8b1c3d86942d6f38990508f69cfae622c
Valantis Kamayiannis <kamayiannis@yahoo.com>
2014-12-09 23:31:28 +0200

    Playlist created as a file

--- playlist.rb
+++ playlist.rb
@@ -1,17 +1,15 @@
-require 'find'
-
 PLAYLIST_EXTENSION = ".m3u"
 SUPPORTED_EXTENSION = ".mp3"
 
 def init_dir_array(parent)
   directories = []
+  sub = []
   Dir.foreach(parent) do |dir|
     dirpath = parent + "\\" + dir
     if File.directory?(dirpath) && dir != "." && dir != ".."
-        #puts dirpath
         directories << dirpath
-        directories + init_dir_array(dirpath)
-      end
+		directories = directories + init_dir_array(dirpath)
+	end
   end
   directories
 end
@@ -24,20 +22,33 @@ print "Enter the filename of the playlist (m3u extension will be added: "
 filename = gets.chomp + PLAYLIST_EXTENSION
 directories = init_dir_array(parent_dir)
 i = 0
-while i < num_of_tracks
-  active_directory = directories[rand(directories.length)]
-  puts "Active directory = #{active_directory}"
-  files = []
-  Find.find(active_directory) do |f|
-    if File.file?(f)
-      fullfilepath = active_directory + "\\" + File.basename(f)
-      if File.extname(fullfilepath) == SUPPORTED_EXTENSION
-        files << fullfilepath
-        #puts files
-      end
-    end
-  end
-  puts files[rand(files.length)]
-  i += 1
+begin
+    file = File.open(filename, "w") 
+	while i < num_of_tracks
+	  active_directory = directories[rand(directories.length)]
+	  #puts "ACTIVE DIRECTORY #{active_directory}"
+	  files = []
+	  mp3s = []
+	  files = Dir.entries(active_directory).select{|f| !File.directory? f}
+	  files.each_index  {|i| files[i] = active_directory + "\\" + files[i] }
+	  files.each do |f|
+	    #For some reason this did not work
+		#if File.extname(f) != SUPPORTED_EXTENSION
+		# files.delete(f)
+		#end
+		#puts f
+		if File.extname(f) == SUPPORTED_EXTENSION
+		  mp3s << f
+		end
+	  end
+	  if mp3s.length > 0 
+	    added_file = mp3s[rand(mp3s.length)] + "\n"
+	    file.write(added_file)
+	    i += 1
+	  end
+	end
+rescue 
+  puts "Error saving file"
+ensure
+   file.close unless file == nil
 end
-
