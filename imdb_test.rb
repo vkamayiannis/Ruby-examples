a568532d3283fc20b93a8e445879dd0260a10e1c
Valantis Kamayiannis <kamayiannis@yahoo.com>
2015-09-17 11:18:31 +0000

    changed imdb_test

--- imdb_test.rb
+++ imdb_test.rb
@@ -9,8 +9,7 @@ doc.css("//table[@class='cast_list']/tr/td").each do |item|
 	# if name != '' 
 	#   puts name
 	# end
-	character = item.css(".character").text
-	puts character
-	#character = doc.at_css("//td[@class='character']/div")
-	#puts "#{name} - #{character}"
+	#character = item.css(".character").text
+	character = doc.at_css("//td[@class='character']/div/a")
+	puts "#{name} - #{character}"
 end
--- yearcalc.rb
+++ yearcalc.rb
@@ -2,4 +2,4 @@ print "What is your name? "
 name = gets.chomp
 print "What is your age? "
 age = gets.chomp.to_i
-puts "#{name} was born in #{Time.now.year - age}"
+puts "#{name} was born in #{Time.now.year - age}"
